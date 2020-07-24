315a9ac787062e1ea8ee8bd09ae1a217255a6d4b
morphinae <32517505+morphinae93@users.noreply.github.com>
2020-07-24 03:32:03 +0200

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -1,25 +1,41 @@
 module.exports = {
-    name: 'role',
-    description: "This adds/removes roles",
-    execute(message, messageArray, guild) {
-      if (message.member.roles.cache.find(r => r.name === "root")) {
-          if (messageArray[2]==="add") {
-            if (!message.guild.roles.cache.find(r => r.name === messageArray[3])){
-              message.channel.send('this role doesnt exist')
-            } else if ( user = message.guild.members.fetch({query: messageArray[5], limit: 1})) {
-              member.addRole(messageArray[3]);
-           }
-          } 
-           if (messageArray[2]==="remove") {
-            message.channel.send('valid command recognised.');
-            if(!message.member.roles.cache.find(r => r.name === messageArray[3])){
-              message.channel.send('this user has no role');
-            } else {
-              member.removeRole(messageArray[3]);
-            } 
-          } else message.channel.send('~~some~~*no*thing went wrong');
+  name: 'role',
+  description: "This add/remove role",
+  execute(message, messageArray, guild) {
+    //verify if user is root
+    if (message.member.roles.cache.find(r => r.name === "root")) {
+        //check if it should add a role
+        if (messageArray[2]==="add") {
+          //check if role exist
+          if (!message.guild.roles.cache.find(r => r.name === messageArray[3])){
+            message.channel.send('this role doesnt exist')
           }
-        }
-      }
-    
-  
+          //if it exists asign it
+          else {
+            const role = message.guild.roles.cache.find(r => r.name === messageArray[3]);
+            let member = message.mentions.members.first()
+            member.roles.add([role])
+            message.channel.send('role added')
+          }
+         }
+        //check if it needs to remove a role
+        else if (messageArray[2]==="remove") {
+
+          //check if the user has the role
+          if (!message.mentions.members.first().roles.cache.find(r => r.name === messageArray[3])){
+            message.channel.send('this user doesnt have this role already')
+          }
+          //if he has it we remove it, we dont check if the role exist because
+          //if the user has it, we remove it, else we exit
+          else if (message.mentions.members.first().roles.cache.find(r => r.name ===messageArray[3])){
+            const role = message.guild.roles.cache.find(r => r.name === messageArray[3]);
+            let member = message.mentions.members.first()
+            member.roles.remove([role])
+            message.channel.send('role removed')
+          }
+        }else{
+            message.channel.send('~~some~~*no*thing went wrong with *you*~~r command~~')
+        };
+
+    }
+  }}
